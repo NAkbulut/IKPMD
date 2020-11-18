c0be44a319d94802e0eda518e96ee95b1bd075ab
Yassir <s1113405@student.hsleiden.nl>
2020-11-18 19:30:15 +0100

    Onclicks fixed for grid items. Two problems found: - Grid items resize automatically instead of being a scrollable list. Preffered size of grid items is observable when 4 grid items are visible. - Still have to fix layout on PokeDetails.java, but that is for later.

--- app/src/main/java/com/example/ikpmd_periode2/MainActivity.java
+++ app/src/main/java/com/example/ikpmd_periode2/MainActivity.java
@@ -1,22 +1,31 @@
 package com.example.ikpmd_periode2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
+    CardView pokemon;
+    CardView pokemon2;
+    CardView pokemon3;
+    CardView pokemon4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
@@ -25,7 +34,11 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-        //yeet
+    }
+
+    public void Switcher(View v){
+        startActivity(new Intent(MainActivity.this,PokeDetails.class));
     }
 
 }
+
