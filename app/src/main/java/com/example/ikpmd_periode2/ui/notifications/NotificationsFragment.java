e95a2cfaa057689e811ec7a6717e000e47cfc264
Yassir <s1113405@student.hsleiden.nl>
2020-11-18 16:34:00 +0100

    Colorchanges are fun!

--- app/src/main/java/com/example/ikpmd_periode2/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/ikpmd_periode2/ui/notifications/NotificationsFragment.java
@@ -22,8 +22,8 @@ public class NotificationsFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         notificationsViewModel =
                 new ViewModelProvider(this).get(NotificationsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
+        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        final TextView textView = root.findViewById(R.id.text_dashboard);
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
